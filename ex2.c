6f0fc098e122f2fe872c06f7632f732f776397ee
Roslyn Parker <magicmachineplaymc@gmail.com>
2019-03-08 09:50:26 -0500

    Add files via upload

--- ex2.c
+++ ex2.c
@@ -1,18 +1,41 @@
 // Roslyn Parker
-// 1 March 2019 
-// Version 1
-// Purpose exercise 2 in lab 5, printing floating point parts
-
+// 8 March 2019 
+// Version 21
+// Purpose: Floating point struct exercise 1
 #include <stdio.h> 
 
 void printSingle(float f); 
 
-void main() { 
+int main() 
+{ 
 	float f = 3.5; 
 	printSingle(f); 
+
+	return 0; 
 } 
 
-void printSingle(float f) { 
-	unsigned int *up = (unsigned int*)&f; //float f; 
-	printf("For value %u, sign = %d, exp = , fraction= \n", up, up, up, up); 
-} 
+void printSingle(float f) 
+{ 
+	// a Floating point number has 3 parts 
+	// The formula for a floating point is (-1)^s M 2^E
+	// We need to print out the sign, exp, and fraction 
+	// sign = first bit, sign bit
+	// exp = e + bias
+	// frac = 1.0 + 0.M
+	// M is the mantissa, normally a fractional value from 1.0-2.0
+	unsigned int* ptr = (unsigned int*)&f; 
+
+	int sign = (*ptr >> 31) & 1; 
+	int exp = (*ptr >> 23) & 0xff; 	
+	int frac = *ptr & 0x7fffff;  
+
+	printf("for value %1.1f,", f); 
+	printf("sign = %x", sign); 
+	printf(", exp = %d", exp); 
+	printf(", frac = %x\n", frac); 
+	 
+	//struct Single s; 
+	//*(float *)&s = f; 
+
+
+}
